6e6fdbc39ed8a30e1a2e7c49be88811f1de74d0f
Peter H. Boling <peter.boling@gmail.com>
2026-06-18 07:39:34 -0600

    ⬆️ Raise kettle-dev floor to 2.2.12

--- gems/kettle-jem/lib/kettle/jem.rb
+++ gems/kettle-jem/lib/kettle/jem.rb
@@ -3459,7 +3459,7 @@ module Kettle
       [
         {name: "appraisal2", source: %(gem "appraisal2", "~> 3.1", ">= 3.1.2"\n)},
         {name: "bundler-audit", source: %(gem "bundler-audit", "~> 0.9.3"\n)},
-        {name: "kettle-dev", source: %(gem "kettle-dev", "~> 2.2", ">= 2.2.11"\n)},
+        {name: "kettle-dev", source: %(gem "kettle-dev", "~> 2.2", ">= 2.2.12"\n)},
         {name: "kettle-drift", source: %(gem "kettle-drift", "~> 1.0", ">= 1.0.3"\n)},
         {name: "kettle-jem", source: %(gem "kettle-jem", "~> 7.0", ">= 7.0.0"\n)},
         {name: "kettle-test", source: %(gem "kettle-test", "~> 2.0", ">= 2.0.5"\n)},
