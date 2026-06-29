ce96f00a74bb606e2c8a8c09023fe9980f2f2bd6
Peter H. Boling <peter.boling@gmail.com>
2026-06-29 03:08:13 -0600

    ⬆️ Raise kettle-dev floor to 2.2.24

--- gems/kettle-jem/spec/kettle/jem_dependency_floor_slice_spec.rb
+++ gems/kettle-jem/spec/kettle/jem_dependency_floor_slice_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe Kettle::Jem do
       },
       "kettle-dev" => {
         declaration_names: ["kettle-dev", "{KJ|KETTLE_DEV_GEM}"],
-        requirement_args: %("~> 2.2", ">= 2.2.23"),
-        lock_version: "2.2.23",
+        requirement_args: %("~> 2.2", ">= 2.2.24"),
+        lock_version: "2.2.24",
         requirement_surfaces: [
           "kettle-jem.gemspec",
           "lib/kettle/jem.rb",
