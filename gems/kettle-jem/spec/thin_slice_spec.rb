951ae6eea22c395c499594f6f576ea0eeebf7259
Peter H. Boling <peter.boling@gmail.com>
2026-07-14 20:26:17 -0600

    ⬆️ Raise turbo_tests2 floor

--- gems/kettle-jem/spec/thin_slice_spec.rb
+++ gems/kettle-jem/spec/thin_slice_spec.rb
@@ -3411,7 +3411,7 @@ RSpec.describe Kettle::Jem do
       expect(gemfile).to include('gem "kettle-test", "~> 2.0", ">= 2.0.10"')
       expect(gemfile.lines.count { |line| line.start_with?('gem "kettle-dev"') }).to eq(1)
       expect(gemfile.lines.count { |line| line.start_with?('gem "kettle-test"') }).to eq(1)
-      expect(gemfile).to include('gem "turbo_tests2", "~> 3.1", ">= 3.1.8"')
+      expect(gemfile).to include('gem "turbo_tests2", "~> 3.1", ">= 3.1.9"')
       expect(rakefile).to include('require "kettle/dev"')
       expect(rakefile).to include("Kettle::Dev.install_tasks")
       expect(rakefile).to include("namespace :family do")
@@ -3466,7 +3466,7 @@ RSpec.describe Kettle::Jem do
       expect(report.fetch(:changed_files)).to include("Gemfile")
       expect(gemfile).to include('gem "kettle-dev", "~> 2.3", ">= 2.3.4"')
       expect(gemfile).to include('gem "kettle-test", "~> 2.0", ">= 2.0.10"')
-      expect(gemfile).to include('gem "turbo_tests2", "~> 3.1", ">= 3.1.8"')
+      expect(gemfile).to include('gem "turbo_tests2", "~> 3.1", ">= 3.1.9"')
     end
   end
 
