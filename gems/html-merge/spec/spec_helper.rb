ed26f5fd6c95636d3a6397a5fee940180159049c
Peter H. Boling <peter.boling@gmail.com>
2026-07-21 18:03:37 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/html-merge/spec/spec_helper.rb
+++ gems/html-merge/spec/spec_helper.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# Config for development dependencies of this library
+# i.e., not configured by this library
+#
+# SimpleCov & related config (must run BEFORE any other requires)
+# NOTE: Gemfiles for non-coverage appraisals may not have kettle-soup-cover.
+#       The rescue LoadError handles that scenario.
+begin
+  require "kettle-soup-cover"
+  if Kettle::Soup::Cover::DO_COV
+    # Requiring simplecov loads the project-local `.simplecov`.
+    require "simplecov"
+    require "kettle/soup/cover/config"
+    SimpleCov.start
+  end
+rescue LoadError => error
+  # check the error message and re-raise when unexpected
+  raise error unless error.message.include?("kettle")
+end
+
+# External RSpec & related config
+require "kettle/test/rspec"
+
 require "html/merge"
 
 RSpec.configure do |config|
