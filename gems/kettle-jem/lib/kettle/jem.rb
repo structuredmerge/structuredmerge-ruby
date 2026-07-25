2cbc056101dac13c8170f4db2238cf1b69e72ad9
Peter H. Boling <peter.boling@gmail.com>
2026-07-25 02:23:06 -0600

    ⬆️ Raise kettle-jem dependency floors

--- gems/kettle-jem/lib/kettle/jem.rb
+++ gems/kettle-jem/lib/kettle/jem.rb
@@ -4538,7 +4538,7 @@ module Kettle
         {name: "appraisal2", source: %(gem "appraisal2", "~> 3.2", ">= 3.2.0"\n)},
         {name: "bundler-audit", source: %(gem "bundler-audit", "~> 0.9.3"\n)},
         {name: "kettle-dev", source: %(gem "kettle-dev", "~> 2.4", ">= 2.4.3"\n)},
-        {name: "kettle-drift", source: %(gem "kettle-drift", "~> 1.0", ">= 1.0.6"\n)},
+        {name: "kettle-drift", source: %(gem "kettle-drift", "~> 1.0", ">= 1.0.7"\n)},
         {name: "kettle-jem", source: %(gem "kettle-jem", "~> 7.0", ">= 7.0.0"\n)},
         {name: "kettle-test", source: %(gem "kettle-test", "~> 2.0", ">= 2.0.15"\n)},
         {name: "rake", source: %(gem "rake", "~> 13.0"\n)},
