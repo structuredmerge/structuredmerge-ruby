c2fdb338d50723dbabd748d7adf8e0b52ce5da26
Peter H. Boling <peter.boling@gmail.com>
2026-07-25 17:31:48 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/ast-merge/spec/ast/merge/version_spec.rb
+++ gems/ast-merge/spec/ast/merge/version_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "anonymous_loader"
+require 'anonymous_loader'
 RSpec.describe Ast::Merge::Version do
   it_behaves_like 'a Version module', described_class
 
-it "executes the version file for coverage without redefining constants" do
-  path = File.expand_path("../../../lib/ast/merge/version.rb", __dir__)
-  anonymous_namespace = AnonymousLoader.load(files: path)
+  it 'executes the version file for coverage without redefining constants' do
+    path = File.expand_path('../../../lib/ast/merge/version.rb', __dir__)
+    anonymous_namespace = AnonymousLoader.load(files: path)
 
-  expect(anonymous_namespace::Ast::Merge::Version::VERSION).to eq(described_class::VERSION)
-end
+    expect(anonymous_namespace::Ast::Merge::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
