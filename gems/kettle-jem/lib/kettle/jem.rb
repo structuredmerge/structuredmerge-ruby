a9f02a4e998a7e1789c138bd83f4c7392da8fcb3
Peter H. Boling <peter.boling@gmail.com>
2026-07-25 22:29:14 -0600

    ⬆️ Raise kettle-jem dependency floors

--- gems/kettle-jem/lib/kettle/jem.rb
+++ gems/kettle-jem/lib/kettle/jem.rb
@@ -4560,8 +4560,8 @@ module Kettle
         {name: "kettle-test", source: %(gem "kettle-test", "~> 2.0", ">= 2.0.15"\n)},
         {name: "rake", source: %(gem "rake", "~> 13.0"\n)},
         {name: "rspec", source: %(gem "rspec", "~> 3.0"\n)},
-        {name: "stone_checksums", source: %(gem "stone_checksums", "~> 1.0", ">= 1.0.6"\n)},
-        {name: "turbo_tests2", source: %(gem "turbo_tests2", "~> 3.2", ">= 3.2.1"\n)}
+        {name: "stone_checksums", source: %(gem "stone_checksums", "~> 1.0", ">= 1.0.7"\n)},
+        {name: "turbo_tests2", source: %(gem "turbo_tests2", "~> 3.2", ">= 3.2.2"\n)}
       ].freeze
     end
 
