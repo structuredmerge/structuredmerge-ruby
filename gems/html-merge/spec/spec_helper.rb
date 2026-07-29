0a0d7e9d97f3b2c7aedd665291d7c076ce8a63a5
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:50:09 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/html-merge/spec/spec_helper.rb
+++ gems/html-merge/spec/spec_helper.rb
@@ -22,7 +22,6 @@ end
 # External RSpec & related config
 require "kettle/test/rspec"
 # `kettle/test/rspec` installs harness helpers documented in spec/README.md.
-
 require "html/merge"
 
 RSpec.configure do |config|
