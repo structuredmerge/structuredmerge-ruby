ea5324cf489708bcc299558278170d5aa42ddc19
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:21:36 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/binary-merge/spec/binary/merge/version_spec.rb
+++ gems/binary-merge/spec/binary/merge/version_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "anonymous_loader"
+require 'anonymous_loader'
 RSpec.describe Binary::Merge::Version do
-  it_behaves_like "a Version module", described_class
+  it_behaves_like 'a Version module', described_class
 
-it "executes the version file for coverage without redefining constants" do
-  path = File.expand_path("../../../lib/binary/merge/version.rb", __dir__)
-  anonymous_namespace = AnonymousLoader.load(files: path)
+  it 'executes the version file for coverage without redefining constants' do
+    path = File.expand_path('../../../lib/binary/merge/version.rb', __dir__)
+    anonymous_namespace = AnonymousLoader.load(files: path)
 
-  expect(anonymous_namespace::Binary::Merge::Version::VERSION).to eq(described_class::VERSION)
-end
+    expect(anonymous_namespace::Binary::Merge::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
--- gems/binary-merge/spec/spec_helper.rb
+++ gems/binary-merge/spec/spec_helper.rb
@@ -21,6 +21,7 @@ end
 
 # External RSpec & related config
 require 'kettle/test/rspec'
+# `kettle/test/rspec` installs harness helpers documented in spec/README.md.
 
 # This library
 require 'binary/merge'
