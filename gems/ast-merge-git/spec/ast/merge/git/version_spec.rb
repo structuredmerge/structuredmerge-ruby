18cda46dbacea28948ec4a2f1f55de80de30f1a0
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:13:59 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

---
+++ gems/ast-merge-git/spec/ast/merge/git/version_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "anonymous_loader"
+RSpec.describe Ast::Merge::Git::Version do
+  it_behaves_like "a Version module", described_class
+
+it "executes the version file for coverage without redefining constants" do
+  path = File.expand_path("../../../../lib/ast/merge/git/version.rb", __dir__)
+  anonymous_namespace = AnonymousLoader.load(files: path)
+
+  expect(anonymous_namespace::Ast::Merge::Git::Version::VERSION).to eq(described_class::VERSION)
+end
+end
--- gems/json-merge/spec/json/merge/version_spec.rb
+++ gems/json-merge/spec/json/merge/version_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "anonymous_loader"
+require 'anonymous_loader'
 RSpec.describe Json::Merge::Version do
-  it_behaves_like "a Version module", described_class
+  it_behaves_like 'a Version module', described_class
 
-it "executes the version file for coverage without redefining constants" do
-  path = File.expand_path("../../../lib/json/merge/version.rb", __dir__)
-  anonymous_namespace = AnonymousLoader.load(files: path)
+  it 'executes the version file for coverage without redefining constants' do
+    path = File.expand_path('../../../lib/json/merge/version.rb', __dir__)
+    anonymous_namespace = AnonymousLoader.load(files: path)
 
-  expect(anonymous_namespace::Json::Merge::Version::VERSION).to eq(described_class::VERSION)
-end
+    expect(anonymous_namespace::Json::Merge::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
