d8c54425d1252ae48a80f5d22c55f202660ff515
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:16:51 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

---
+++ gems/html-merge/spec/html/merge/version_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "anonymous_loader"
+RSpec.describe Html::Merge::Version do
+  it_behaves_like "a Version module", described_class
+
+it "executes the version file for coverage without redefining constants" do
+  path = File.expand_path("../../../lib/html/merge/version.rb", __dir__)
+  anonymous_namespace = AnonymousLoader.load(files: path)
+
+  expect(anonymous_namespace::Html::Merge::Version::VERSION).to eq(described_class::VERSION)
+end
+end
