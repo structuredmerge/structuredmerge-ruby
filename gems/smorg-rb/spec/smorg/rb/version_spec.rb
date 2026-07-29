ec40950ee7dfb031786cab904339989dc4228952
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:22:55 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/smorg-rb/spec/smorg/rb/version_spec.rb
+++ gems/smorg-rb/spec/smorg/rb/version_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "anonymous_loader"
+require 'anonymous_loader'
 RSpec.describe Smorg::RB::Version do
-  it_behaves_like "a Version module", described_class
+  it_behaves_like 'a Version module', described_class
 
-it "executes the version file for coverage without redefining constants" do
-  path = File.expand_path("../../../lib/smorg/rb/version.rb", __dir__)
-  anonymous_namespace = AnonymousLoader.load(files: path)
+  it 'executes the version file for coverage without redefining constants' do
+    path = File.expand_path('../../../lib/smorg/rb/version.rb', __dir__)
+    anonymous_namespace = AnonymousLoader.load(files: path)
 
-  expect(anonymous_namespace::Smorg::RB::Version::VERSION).to eq(described_class::VERSION)
-end
+    expect(anonymous_namespace::Smorg::RB::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
