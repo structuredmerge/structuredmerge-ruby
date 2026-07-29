a0d1ca94276bf70334ac7807c8472e3d4d5097b6
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 21:14:21 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/plain-merge/spec/plain/merge/version_spec.rb
+++ gems/plain-merge/spec/plain/merge/version_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "anonymous_loader"
+require 'anonymous_loader'
 RSpec.describe Plain::Merge::Version do
-  it_behaves_like "a Version module", described_class
+  it_behaves_like 'a Version module', described_class
 
-it "executes the version file for coverage without redefining constants" do
-  path = File.expand_path("../../../lib/plain/merge/version.rb", __dir__)
-  anonymous_namespace = AnonymousLoader.load(files: path)
+  it 'executes the version file for coverage without redefining constants' do
+    path = File.expand_path('../../../lib/plain/merge/version.rb', __dir__)
+    anonymous_namespace = AnonymousLoader.load(files: path)
 
-  expect(anonymous_namespace::Plain::Merge::Version::VERSION).to eq(described_class::VERSION)
-end
+    expect(anonymous_namespace::Plain::Merge::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
