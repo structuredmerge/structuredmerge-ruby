114a840407a0d0c90ebdb9e95e99e3b87aeffcbc
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 11:40:06 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- gems/toml-merge/lib/toml/merge/version.rb
+++ gems/toml-merge/lib/toml/merge/version.rb
@@ -5,7 +5,7 @@ module Toml
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.0'
+      VERSION = "7.1.0"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
