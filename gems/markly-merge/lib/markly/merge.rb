0b3a6962084c64119eab029bab8f3a129450668d
Peter H. Boling <peter.boling@gmail.com>
2026-08-02 16:18:06 -0600

    🎨 Template markly-merge by kettle-family

--- gems/markly-merge/lib/markly/merge.rb
+++ gems/markly-merge/lib/markly/merge.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'version_gem'
-require_relative 'merge/version'
 
 require 'markdown-merge'
 require 'markly'
+require_relative 'merge/version'
 
 module Markly
   module Merge
