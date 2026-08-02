dacff7e5f19dba39cf8b6593c309cd2c31c9a48b
Peter H. Boling <peter.boling@gmail.com>
2026-08-02 16:17:16 -0600

    🎨 Template kramdown-merge by kettle-family

--- gems/kramdown-merge/lib/kramdown/merge.rb
+++ gems/kramdown-merge/lib/kramdown/merge.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'version_gem'
-require_relative 'merge/version'
 
 require 'markdown-merge'
 require 'kramdown'
+require_relative 'merge/version'
 
 module Kramdown
   module Merge
