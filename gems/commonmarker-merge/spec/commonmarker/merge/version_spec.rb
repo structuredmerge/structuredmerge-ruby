5e869385df05717ddfb624b50a6b35aa49c8313c
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 18:48:47 -0600

    🎨 Template commonmarker-merge by kettle-family

--- gems/commonmarker-merge/spec/commonmarker/merge/version_spec.rb
+++ gems/commonmarker-merge/spec/commonmarker/merge/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
+require 'commonmarker/merge'
 RSpec.describe Commonmarker::Merge::Version do
   it_behaves_like 'a Version module', described_class
 
