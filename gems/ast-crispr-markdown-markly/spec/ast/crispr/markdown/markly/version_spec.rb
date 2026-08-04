a97662a74ccbb5f06a907dfad67386d5f8efb475
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 23:21:10 -0600

    🎨 Template ast-crispr-markdown-markly by kettle-family

--- gems/ast-crispr-markdown-markly/spec/ast/crispr/markdown/markly/version_spec.rb
+++ gems/ast-crispr-markdown-markly/spec/ast/crispr/markdown/markly/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
-require 'ast/crispr/markdown/markly'
+require 'ast-crispr-markdown-markly'
 RSpec.describe Ast::Crispr::Markdown::Markly::Version do
   it_behaves_like 'a Version module', described_class
 
