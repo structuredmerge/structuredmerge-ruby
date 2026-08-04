cb5486893dd0b910d05cca65094e7ef96b7c77e0
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 18:34:52 -0600

    🎨 Template dotenv-merge by kettle-family

--- gems/dotenv-merge/spec/dotenv/merge/version_spec.rb
+++ gems/dotenv-merge/spec/dotenv/merge/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
+require 'dotenv/merge'
 RSpec.describe Dotenv::Merge::Version do
   it_behaves_like 'a Version module', described_class
 
