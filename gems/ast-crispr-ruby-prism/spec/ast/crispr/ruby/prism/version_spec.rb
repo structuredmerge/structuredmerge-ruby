5c2f0691c8027714df4d7109baf05546e7e41fb2
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 23:17:40 -0600

    🎨 Template ast-crispr-ruby-prism by kettle-family

--- gems/ast-crispr-ruby-prism/spec/ast/crispr/ruby/prism/version_spec.rb
+++ gems/ast-crispr-ruby-prism/spec/ast/crispr/ruby/prism/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
-require 'ast/crispr/ruby/prism'
+require 'ast-crispr-ruby-prism'
 RSpec.describe Ast::Crispr::Ruby::Prism::Version do
   it_behaves_like 'a Version module', described_class
 
