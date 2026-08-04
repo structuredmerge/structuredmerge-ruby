5d8206b8c87e03ae4f8010e80dd8a37776b2727b
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 23:55:34 -0600

    🎨 Template smorg-rb by kettle-family

--- gems/smorg-rb/spec/smorg/rb/version_spec.rb
+++ gems/smorg-rb/spec/smorg/rb/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
-require 'smorg/rb'
+require 'smorg-rb'
 RSpec.describe Smorg::RB::Version do
   it_behaves_like 'a Version module', described_class
 
