3026b438f7304961790f0ea3d2e9df3ccb8cf325
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 23:15:18 -0600

    🎨 Template ruby-merge by kettle-family

--- gems/ruby-merge/spec/ruby/merge/version_spec.rb
+++ gems/ruby-merge/spec/ruby/merge/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
-require 'ruby/merge'
+require 'ruby-merge'
 RSpec.describe Ruby::Merge::Version do
   it_behaves_like 'a Version module', described_class
 
