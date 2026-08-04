672c18b472fdf5327ddc3de767da6911e7b9d568
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 18:31:51 -0600

    🎨 Template ast-merge-git by kettle-family

--- gems/ast-merge-git/spec/ast/merge/git/version_spec.rb
+++ gems/ast-merge-git/spec/ast/merge/git/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'anonymous_loader'
+require 'ast/merge/git'
 RSpec.describe Ast::Merge::Git::Version do
   it_behaves_like 'a Version module', described_class
 
