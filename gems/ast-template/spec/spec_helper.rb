7526566554ee15e47383bf2aee3166feedd5192c
Peter H. Boling <peter.boling@gmail.com>
2026-08-06 23:08:37 -0600

    🧹 Remove unrelated adapter loads from ast-template specs

--- gems/ast-template/spec/spec_helper.rb
+++ gems/ast-template/spec/spec_helper.rb
@@ -28,10 +28,6 @@ require 'ast/merge'
 require 'ast/merge/rspec'
 require 'ast/template'
 
-require 'markly-merge'
-require 'prism-merge'
-require 'toml-merge'
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
