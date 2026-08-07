e8d9b67c6ba135a4ac6511fb4ea8b53781802472
Peter H. Boling <peter.boling@gmail.com>
2026-08-06 22:06:59 -0600

    🎨 Template ast-crispr-markdown-markly by kettle-family

--- gems/ast-crispr-markdown-markly/spec/spec_helper.rb
+++ gems/ast-crispr-markdown-markly/spec/spec_helper.rb
@@ -24,7 +24,8 @@ require 'kettle/test/rspec'
 # `kettle/test/rspec` installs harness helpers documented in spec/README.md.
 
 # This library
-require 'ast-crispr-markdown-markly'
+require 'ast/crispr/markdown/markly'
+
 require 'ast/crispr/rspec'
 
 RSpec.configure do |config|
