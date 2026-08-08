16d7807c564e8034c00eb95abe3be08abdcd1056
Peter H. Boling <peter.boling@gmail.com>
2026-08-07 19:43:39 -0600

    🔖 Bump gem version

--- gems/tree_haver/lib/tree_haver/version.rb
+++ gems/tree_haver/lib/tree_haver/version.rb
@@ -4,7 +4,7 @@ module TreeHaver
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = '7.1.0'
+    VERSION = '7.1.1'
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
