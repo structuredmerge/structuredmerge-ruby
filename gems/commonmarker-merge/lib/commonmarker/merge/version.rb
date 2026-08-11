b18a9aa79ff58a00ac92d46c4e6272213851ea94
Peter H. Boling <peter.boling@gmail.com>
2026-08-11 03:30:04 -0600

    🔖 Bump gem version

--- gems/commonmarker-merge/lib/commonmarker/merge/version.rb
+++ gems/commonmarker-merge/lib/commonmarker/merge/version.rb
@@ -5,7 +5,7 @@ module Commonmarker
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.2'
+      VERSION = '7.1.3'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
