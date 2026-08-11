60b00986105a90bc60695a30553b9619c8a970f2
Peter H. Boling <peter.boling@gmail.com>
2026-08-10 18:28:15 -0600

    🔖 Bump gem version

--- gems/toml-merge/lib/toml/merge/version.rb
+++ gems/toml-merge/lib/toml/merge/version.rb
@@ -5,7 +5,7 @@ module Toml
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.1'
+      VERSION = '7.1.2'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
