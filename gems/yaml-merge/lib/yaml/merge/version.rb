b1cc2f95f68d4d28fbe103052255fcbc415d775d
Peter H. Boling <peter.boling@gmail.com>
2026-08-16 07:53:20 -0400

    🔖 Bump gem version

--- gems/yaml-merge/lib/yaml/merge/version.rb
+++ gems/yaml-merge/lib/yaml/merge/version.rb
@@ -5,7 +5,7 @@ module Yaml
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.3'
+      VERSION = '7.1.4'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
