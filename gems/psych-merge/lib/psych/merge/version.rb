e78ffcac6cfee748473e8251019bcf1ce553ddc2
Peter H. Boling <peter.boling@gmail.com>
2026-08-30 02:06:21 -0400

    🔖 Bump gem version

--- gems/psych-merge/lib/psych/merge/version.rb
+++ gems/psych-merge/lib/psych/merge/version.rb
@@ -5,7 +5,7 @@ module Psych
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.4'
+      VERSION = '7.1.5'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
