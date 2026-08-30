07c671c5a0f1adf4e165f15c0ab47a0fbeb9db1e
Peter H. Boling <peter.boling@gmail.com>
2026-08-30 17:24:29 -0400

    🔖 Bump gem version

--- gems/ast-merge-git/lib/ast/merge/git/version.rb
+++ gems/ast-merge-git/lib/ast/merge/git/version.rb
@@ -6,7 +6,7 @@ module Ast
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = '7.1.5'
+        VERSION = '7.1.6'
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
