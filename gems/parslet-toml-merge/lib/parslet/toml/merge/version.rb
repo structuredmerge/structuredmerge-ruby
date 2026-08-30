e6c9200eabe7a8fb793b3e57a1a00bc2d2c53c09
Peter H. Boling <peter.boling@gmail.com>
2026-08-30 02:06:21 -0400

    🔖 Bump gem version

--- gems/parslet-toml-merge/lib/parslet/toml/merge/version.rb
+++ gems/parslet-toml-merge/lib/parslet/toml/merge/version.rb
@@ -6,7 +6,7 @@ module Parslet
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = '7.1.4'
+        VERSION = '7.1.5'
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
