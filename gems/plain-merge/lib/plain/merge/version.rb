70229860c160375b1f5a83d0b71d46742c84b9aa
Peter H. Boling <peter.boling@gmail.com>
2026-09-01 21:18:26 -0400

    🔖 Bump gem version

--- gems/plain-merge/lib/plain/merge/version.rb
+++ gems/plain-merge/lib/plain/merge/version.rb
@@ -5,7 +5,7 @@ module Plain
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.6'
+      VERSION = '7.1.7'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
