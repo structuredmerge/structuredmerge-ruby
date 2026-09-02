b6e6318c1aa3f4fdf51816e02cb6c1faebf6f7ad
Peter H. Boling <peter.boling@gmail.com>
2026-09-01 21:18:29 -0400

    🔖 Bump gem version

--- gems/ast-template/lib/ast/template/version.rb
+++ gems/ast-template/lib/ast/template/version.rb
@@ -5,7 +5,7 @@ module Ast
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = '7.1.6'
+      VERSION = '7.1.7'
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
